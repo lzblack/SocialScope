bc47fa35476fa6a54f58cef20d95449a04aa435f
Zhi Li <lzblack@users.noreply.github.com>
2024-10-16 22:15:02 -0400

    Optimize sentiment_roberta

--- socialscope/services/sentiment_analysis.py
+++ socialscope/services/sentiment_analysis.py
@@ -1,20 +1,53 @@
+from socialscope.services.sentiment_openai import analyze_sentiment_openai
 from socialscope.services.sentiment_roberta import analyze_sentiment_roberta
 from socialscope.services.sentiment_textblob import analyze_sentiment_textblob
+
+
+def preprocess(text):
+    """"""
+    new_text = []
+    for t in text.split(" "):
+        # t = "@user" if t.startswith("@") and len(t) > 1 else t
+        t = "" if t.startswith("@") and len(t) > 1 else t
+        t = "" if t.startswith("http") else t
+        new_text.append(t)
+    return " ".join(new_text)
 
 
 def analyze_dataframe(df):
     """
     Perform all the sentiment analyses on the text data in the given DataFrame.
     """
-    df["sentiment_analysis_textblob"] = df["text"].apply(analyze_sentiment_textblob)
-    df["sentiment_textblob"] = df["sentiment_analysis_textblob"].apply(lambda x: x["sentiment"])
-    df["polarity_textblob"] = df["sentiment_analysis_textblob"].apply(lambda x: x["polarity"])
+    df["preprocess_text"] = df["text"].apply(preprocess)
+    df["sentiment_analysis_textblob"] = df["preprocess_text"].apply(
+        analyze_sentiment_textblob
+    )
+    df["sentiment_textblob"] = df["sentiment_analysis_textblob"].apply(
+        lambda x: x["sentiment"]
+    )
+    df["polarity_textblob"] = df["sentiment_analysis_textblob"].apply(
+        lambda x: x["polarity"]
+    )
     df["subjectivity_textblob"] = df["sentiment_analysis_textblob"].apply(
         lambda x: x["subjectivity"]
     )
 
-    df["sentiment_analysis_roberta"] = df["text"].apply(analyze_sentiment_roberta)
-    df["sentiment_roberta"] = df["sentiment_analysis_roberta"].apply(lambda x: x["label"])
+    df["sentiment_analysis_roberta"] = df["preprocess_text"].apply(
+        analyze_sentiment_roberta
+    )
+    df["sentiment_roberta"] = df["sentiment_analysis_roberta"].apply(
+        lambda x: x["label"]
+    )
     df["score_roberta"] = df["sentiment_analysis_roberta"].apply(lambda x: x["score"])
+
+    df["sentiment_analysis_openai"] = df["preprocess_text"].apply(
+        analyze_sentiment_openai
+    )
+    df["sentiment_openai"] = df["sentiment_analysis_openai"].apply(
+        lambda x: x["sentiment_openai"]
+    )
+    df["confidence_openai"] = df["sentiment_analysis_openai"].apply(
+        lambda x: x["confidence"]
+    )
 
     return df
--- socialscope/services/sentiment_roberta.py
+++ socialscope/services/sentiment_roberta.py
@@ -1,106 +1,72 @@
+import os
+from pathlib import Path
 import numpy as np
 from scipy.special import softmax
-from transformers import (
-    AutoConfig,
-    AutoModelForSequenceClassification,
-    AutoTokenizer,
-)
-import os
-import logging
-from pathlib import Path
-import shutil
-import requests
-
-logging.basicConfig(level=logging.INFO)
-logging.disable(logging.CRITICAL)
-logger = logging.getLogger(__name__)
+from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
+import threading
 
 
-def preprocess(text):
-    """"""
-    new_text = []
-    for t in text.split(" "):
-        # t = "@user" if t.startswith("@") and len(t) > 1 else t
-        t = "" if t.startswith("@") and len(t) > 1 else t
-        t = "" if t.startswith("http") else t
-        new_text.append(t)
-    return " ".join(new_text)
+class SentimentAnalyzer:
+    _instance = None
+    _lock = threading.Lock()
 
+    def __new__(cls):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance = super(SentimentAnalyzer, cls).__new__(cls)
+                    cls._instance.tokenizer = None
+                    cls._instance.model = None
+                    cls._instance.config = None
+        return cls._instance
 
-def download_file(url, filename):
-    """"""
-    response = requests.get(url, stream=True)
-    total_size = int(response.headers.get("content-length", 0))
-    block_size = 1024  # 1 KB
-    with open(filename, "wb") as file, logging.StreamHandler() as handler:
-        for data in response.iter_content(block_size):
-            size = file.write(data)
-            handler.stream.write(
-                f"\rDownloading {filename}: {size/total_size*100:.2f}%"
-            )
-            handler.stream.flush()
-    print()
+    def load_model(self, model_name: str, cache_dir: str):
+        if self.tokenizer is None or self.model is None or self.config is None:
+            local_model_path = Path(cache_dir) / model_name.split("/")[-1]
 
+            if local_model_path.exists():
+                print(f"Use local model: {local_model_path}")
+                self.tokenizer = AutoTokenizer.from_pretrained(str(local_model_path))
+                self.model = AutoModelForSequenceClassification.from_pretrained(
+                    str(local_model_path)
+                )
+                self.config = AutoConfig.from_pretrained(str(local_model_path))
+            else:
+                print(f"Download from HuggingFace: {model_name}")
+                self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+                self.model = AutoModelForSequenceClassification.from_pretrained(
+                    model_name
+                )
+                self.config = AutoConfig.from_pretrained(model_name)
 
-def load_or_download_model(model_name: str, cache_dir: str):
-    """"""
-    local_model_path = Path(cache_dir) / model_name.split("/")[-1]
+                local_model_path.mkdir(parents=True, exist_ok=True)
+                self.tokenizer.save_pretrained(str(local_model_path))
+                self.model.save_pretrained(str(local_model_path))
+                self.config.save_pretrained(str(local_model_path))
+                print(f"Model is saved to: {local_model_path}")
 
-    if local_model_path.exists():
-        logger.info(f"Use local model: {local_model_path}")
-        try:
-            tokenizer = AutoTokenizer.from_pretrained(str(local_model_path))
-            model = AutoModelForSequenceClassification.from_pretrained(
-                str(local_model_path)
-            )
-            config = AutoConfig.from_pretrained(str(local_model_path))
-            return tokenizer, model, config
-        except Exception as e:
-            logger.warning(f"Failed to load local model: {e}")
-            logger.info("Try to download the model again.")
-            shutil.rmtree(local_model_path, ignore_errors=True)
-
-    logger.info(f"Download from HuggingFace: {model_name}")
-    try:
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = AutoModelForSequenceClassification.from_pretrained(model_name)
-        config = AutoConfig.from_pretrained(model_name)
-
-        local_model_path.mkdir(parents=True, exist_ok=True)
-        tokenizer.save_pretrained(str(local_model_path))
-        model.save_pretrained(str(local_model_path))
-        config.save_pretrained(str(local_model_path))
-        logger.info(f"Model is saved to: {local_model_path}")
-
-        return tokenizer, model, config
-    except Exception as e:
-        logger.error(f"Fail to download: {e}")
-        raise
-
-
-def analyze_sentiment_roberta(text: str, custom_cache_dir: str = None) -> dict:
-    """
-    Perform sentiment analysis on the given text using RoBERTa.
-    参考: https://huggingface.co/cardiffnlp/twitter-roberta-base-sentiment-latest
-    """
-    MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
-    cache_dir = custom_cache_dir or os.path.join(os.getcwd(), ".cache")
-
-    try:
-        tokenizer, model, config = load_or_download_model(MODEL, cache_dir)
-
-        text = preprocess(text)
-        encoded_input = tokenizer(text, return_tensors="pt")
-        output = model(**encoded_input)
+    def analyze_sentiment(self, text: str) -> dict:
+        encoded_input = self.tokenizer(text, return_tensors="pt")
+        output = self.model(**encoded_input)
         scores = output[0][0].detach().numpy()
         scores = softmax(scores)
         max_index = np.argmax(scores)
-        max_label = config.id2label[max_index]
+        max_label = self.config.id2label[max_index]
         max_score = scores[max_index]
-        result = {"label": max_label, "score": np.round(float(max_score), 4)}
-        return result
+        return {"label": max_label, "score": np.round(float(max_score), 4)}
+
+
+def analyze_sentiment_roberta(text: str, custom_cache_dir: str = None) -> dict:
+    MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+    cache_dir = custom_cache_dir or os.path.join(os.getcwd(), ".cache")
+
+    analyzer = SentimentAnalyzer()
+    analyzer.load_model(MODEL, cache_dir)
+
+    try:
+        return analyzer.analyze_sentiment(text)
     except Exception as e:
-        logger.error(f"Error: {str(e)}")
+        print(f"Error: {str(e)}")
         return {"error": str(e)}
 
 
